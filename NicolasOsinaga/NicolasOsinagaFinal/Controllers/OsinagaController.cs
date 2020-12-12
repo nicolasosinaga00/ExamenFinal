1615a6a640110707c5173eabecd14b16c80622c3
nicolasosinaga00 <a2019114771@estudiantes.upsa.edu.bo>
2020-12-12 10:59:04 -0400

    Se creo el controlador

---
+++ NicolasOsinaga/NicolasOsinagaFinal/Controllers/OsinagaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NicolasOsinagaFinal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OsinagaController : ControllerBase
+    {
+        [HttpGet]
+        public string operaciones(int numero)
+        {
+            if (numero > 0)
+                return "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg";
+            else if (numero == 0)
+                return "Realizado por Nicolas Osinaga";
+            else return "ERROR";
+        }
+    }
+}
